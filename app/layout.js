a592b334e290120984cf3e3319db584b311e49fa
Teddi <tedthedude27@gmail.com>
2024-01-17 14:43:47 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={GeistSans}>{children}</body>
+      <CookiesProvider>
+        <body className={GeistSans}>{children}</body>
+      </CookiesProvider>
     </html>
   )
 }
