54d3619ac413d71e8cd78c06df2fbe36102059f3
Teddi <tedthedude27@gmail.com>
2024-04-02 18:00:52 -0500

    cookies fix & footer

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
-"use client"
+"use client";
 
+import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -9,10 +11,12 @@ import { Plus } from "lucide-react";
 import { useCookies } from "next-client-cookies";
 
 export default function Home() {
-  const cookies = useCookies()
+  const cookies = useCookies();
 
+  const expires = new Date();
+  expires.setDate(expires.getDate() + 30);
 
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState("");
   const [tasks, setTasks] = useState(cookies.get("tasks")?.split(",") || []);
 
   useEffect(() => {
@@ -25,51 +29,65 @@ export default function Home() {
   }, [cookies.get("tasks")]);
 
   const addTask = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const formData = new FormData(e.target)
-    const task = formData.get("task")
+    const formData = new FormData(e.target);
+    const task = formData.get("task");
 
-    setInput("")
+    setInput("");
     const updatedTasks = [...tasks, task];
-    cookies.set('tasks', updatedTasks.join(","));
-  }
+    cookies.set("tasks", updatedTasks.join(","), { expires });
+  };
 
   const removeTask = (index) => {
     const updatedTasks = tasks.filter((_, idx) => idx !== index);
-    cookies.set('tasks', updatedTasks.join(","));
-  }
+    cookies.set("tasks", updatedTasks.join(","), { expires });
+  };
 
   return (
-      <main className="text-center p-4">
-        <h1 className="text-4xl">Tasklist</h1>
-        <Separator className="my-4" />
-        <div className="flex justify-center items-center flex-col">
-          <form className="flex w-full max-w-sm items-center space-x-2 justify-center" onSubmit={addTask}>
-            <Input
-              type="text"
-              name="task"
-              placeholder="Enter a task..."
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-            <Button
-              type="submit"
-              variant="outline"
-              size="icon"
-              >
-              <Plus className="h-4 w-4" />
-            </Button>
-          </form>
-          <div className="flex w-full max-w-sm flex-col items-center">
-            {tasks.map((task, index) => (
-              <div className="w-full my-2 flex items-center justify-center" key={index}>
-                <Checkbox id={`task-${index}`} onClick={() => removeTask(index)} />
-                <label className="p-4">{task}</label>
-              </div>
-              ))}
-          </div>
+    <main className="p-4 text-center">
+      <h1 className="text-4xl">Tasklist</h1>
+      <Separator className="my-4" />
+      <div className="flex flex-col items-center justify-center">
+        <form
+          className="flex w-full max-w-sm items-center justify-center space-x-2"
+          onSubmit={addTask}
+        >
+          <Input
+            type="text"
+            name="task"
+            placeholder="Enter a task..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <Button type="submit" variant="outline" size="icon">
+            <Plus className="h-4 w-4" />
+          </Button>
+        </form>
+        <div className="flex w-full max-w-sm flex-col items-center">
+          {tasks.map((task, index) => (
+            <div
+              className="my-2 flex w-full items-center justify-center"
+              key={index}
+            >
+              <Checkbox
+                id={`task-${index}`}
+                onClick={() => removeTask(index)}
+              />
+              <label className="p-4">{task}</label>
+            </div>
+          ))}
         </div>
-      </main>
-    );
+      </div>
+      <div className="mt-auto">
+        <Separator />
+        <p className="py-2 text-xs">Made with ❤️</p>
+        <div className="flex justify-center">
+          <Link href="https://github.com/teddinotteddy">
+            <Image src="/github.svg" width={15} height={15} />
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
